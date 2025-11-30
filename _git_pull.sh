350212085ad5fd6f3c55e95237bf011f64b2a88b
Pyankov Aleksey Mikhaylovich <APyankov@sportmasterlab.net>
2025-11-30 16:46:52 +0300

    draft

--- _git_pull.sh
+++ _git_pull.sh
@@ -1,10 +1,10 @@
 # put this file one folder up to git-controlled
 cd crudio
 
-./git reset --hard
+git reset --hard
 
-./git checkout develop
-./git pull
+git checkout develop
+git pull
 
 chmod +x ./*.sh
 chmod +x ./backend/*.sh
